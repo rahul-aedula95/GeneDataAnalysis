8b1dfa6ba6f5a1cd9a31ba2b0bdc99da209bf698
rahul-aedula95 <rahulaedula95@gmail.com>
2019-09-26 23:37:55 -0600

    First Commit

---
+++ analyze.py
@@ -0,0 +1,23 @@
+import pandas as pd 
+
+
+
+
+class geneExtract:
+
+	def __init__(self):
+
+		df = pd.read_csv('~/Independent_study/Data/datOwnDatasetTranspose.csv')
+
+		print (df)
+
+
+
+
+
+if __name__ == '__main__':
+
+
+	x = geneExtract()
+
+
