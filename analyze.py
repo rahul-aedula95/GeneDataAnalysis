37d28d666463762007ddd331fc250a00986e206f
rahul-aedula95 <rahulaedula95@gmail.com>
2019-09-28 11:00:10 -0600

    added pca

--- analyze.py
+++ analyze.py
@@ -1,5 +1,8 @@
 import pandas as pd 
-
+import numpy as np
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 
@@ -9,7 +12,35 @@ class geneExtract:
 
 		df = pd.read_csv('~/Independent_study/Data/datOwnDatasetTranspose.csv')
 
-		print (df)
+		X = df.values
+		
+		X = self.preprocess_remove_text(X)
+
+		X_pca = self.pca_transformer(X)
+
+
+
+
+
+		
+	def preprocess_remove_text(self,X):
+		
+		x_new = np.delete(X,[0],axis=1)
+
+		return (x_new)
+
+
+	def pca_transformer(self,X):
+
+		pca = PCA(n_components=2)
+		x_new = pca.fit_transform()
+
+		return (x_new)
+
+
+	def pca_plotter(self,X):
+
+
 
 
 
